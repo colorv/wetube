5b411c893010fccc1ba43e6c541c3eb2b060c5f2
colorv <nexsblim@naver.com>
2021-03-26 17:53:04 +0900

    create fake db and update pages

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # WeTuebe
 
 Cloning Youtube with Vanilla and NodeJS
+
+## pages:
+
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [x] Edit Profile
+- [x] Change Password
+- [x] Upload
+- [ ] Video Detail
+- [x] Edit Video
---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 324393,
+    title: "Video awesome",
+    description: "This is something Video",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "SuHyeok",
+      email: "SuHyeok@las.com",
+    },
+  },
+  {
+    id: 223611,
+    title: "Video flower",
+    description: "This is something Video",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "SuHyeok",
+      email: "SuHyeok@las.com",
+    },
+  },
+  {
+    id: 338251,
+    title: "Video ocean",
+    description: "This is something Video",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "SuHyeok",
+      email: "SuHyeok@las.com",
+    },
+  },
+  {
+    id: 915223,
+    title: "Video mountain",
+    description: "This is something Video",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "SuHyeok",
+      email: "SuHyeok@las.com",
+    },
+  },
+];
