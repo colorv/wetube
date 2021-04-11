21cdd0e837f2418410486045c99c59652c674fc7
colorv <nexsblim@naver.com>
2021-04-12 04:50:24 +0900

    add socialLogin

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,25 +1,58 @@
 import express from "express";
+import passport from "passport";
 import {
+  facebookLogin,
   getJoin,
   getlogin,
+  getMe,
+  githubLogin,
   logout,
+  postFacebookLogin,
+  postGithubLogin,
   postJoin,
   postLogin,
 } from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
-import { onlyPublic } from "../middlewares";
+import { onlyPrivate, onlyPublic } from "../middlewares";
 import routes from "../routes";
 
 const globalRouter = express.Router();
 
+// Join
 globalRouter.get(routes.join, onlyPublic, getJoin);
 globalRouter.post(routes.join, onlyPublic, postJoin, postLogin);
 
+// Login
 globalRouter.get(routes.login, onlyPublic, getlogin);
 globalRouter.post(routes.login, onlyPublic, postLogin);
 
+// home
 globalRouter.get(routes.home, home);
+
+// search
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.logout, logout);
+
+// Logout
+globalRouter.get(routes.logout, onlyPrivate, logout);
+
+// github login
+globalRouter.get(routes.gitHub, githubLogin);
+
+globalRouter.get(
+  routes.githubCallback,
+  passport.authenticate("github", { failureRedirect: "/login" }),
+  postGithubLogin
+);
+
+// facebook Login
+globalRouter.get(routes.facebook, facebookLogin);
+
+globalRouter.get(
+  routes.facebookCallback,
+  passport.authenticate("facebook", { failureRedirect: "/login" }),
+  postFacebookLogin
+);
+
+globalRouter.get(routes.me, getMe);
 
 export default globalRouter;
